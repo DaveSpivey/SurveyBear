2dc65e624707050ab2a5394a85f28264762a45b7
DaveSpivey <dspive1@gmail.com>
2016-02-15 08:50:56 -0800

    Fix survey completion processing loop for response generation

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -8,12 +8,12 @@ get '/surveys/:id' do
 end
 
 post '/surveys/:id' do
-  puts params
-  puts params[:id]
+
   survey_taken = Survey.find(params[:id])
-  
-  params[:responses].values do |value| 
+
+  params[:responses].values.each do |value|
   	Response.create(survey_id: params[:id], taker_id: session[:user_id], choice_id: value.to_i)
+    puts value
   end
 
   redirect '/'
