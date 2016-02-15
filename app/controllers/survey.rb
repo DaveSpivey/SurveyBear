c739eab99bb606e83e1c33db84c3648c8fa4e5bd
Lindsey Ullman <lindsey.ullman@gmail.com>
2016-02-14 20:07:11 -0800

    Update post rout for survey submission

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -5,7 +5,12 @@ end
 
 post '/surveys/:id' do
   puts params
-
+  puts params[:id]
+  survey_taken = Survey.find(params[:id])
+  
+  params[:responses].values do |value| 
+  	Response.create(survey_id: params[:id], taker_id: session[:user_id], choice_id: value.to_i)
+  end
 
   redirect '/'
 end
